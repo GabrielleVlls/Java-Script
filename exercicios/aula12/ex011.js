61ead608b7962a831a27de5ae8056a5d4b2a46e9
Gabrielle Vitória Lima <gabriellevitoriadev@gmail.com>
2023-05-08 18:17:33 -0300

    atualização

--- exercicios/aula12/ex011.js
+++ exercicios/aula12/ex011.js
@@ -1,7 +1,7 @@
 var idade = 69
 if (idade < 16) {
     console.log('Não vota')
-} else if ( idade < 18  idade > 65) {
+} else if ( idade < 18 ||idade > 65) {
         console.log('Voto opicional')
     } else if (idade >=18) {
         console.log('Voto obrigatório')
